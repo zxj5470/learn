cfd279046d7863a30d4eb699eb9bde5154de290b
zxj5470 <zxj5470@foxmail.com>
2018-01-25 02:43:21 +0800

    [julia] first

---
+++ julia/learn-before-plugin/j.jl
@@ -0,0 +1,30 @@
+function mandel(z)
+    c = z
+    maxiter = 80
+    for n = 1:maxiter
+        if abs2(z) > 4
+            return n-1
+        end
+        z = z^2 + c
+    end
+    return maxiter
+end
+
+mandel(233)
+
+function randmatstat(t)
+    n = 5
+    v = zeros(t)
+    w = zeros(t)
+    for i = 1:t
+        a = randn(n,n)
+        b = randn(n,n)
+        c = randn(n,n)
+        d = randn(n,n)
+        P = [a b c d]
+        Q = [a b; c d]
+        v[i] = trace((P.'*P)^4)
+        w[i] = trace((Q.'*Q)^4)
+    end
+    std(v)/mean(v), std(w)/mean(w)
+end
