f90a87ee85c8c55bbb697c964e73602e0bb51c1b
zxj5470 <zxj5470@foxmail.com>
2018-03-09 20:55:59 +0800

    [CSharp]  GUI play in .NET Core

---
+++ csharp/bnd/ZxGUI/MainWindow.cs
@@ -0,0 +1,30 @@
+﻿using DevZH.UI;
+using DevZH.UI.Drawing;
+
+namespace bnd.ZxGUI
+{
+    public class MainWindow : Window
+    {
+        private HorizontalBox _mainBox;
+        private Tab _outerTab;
+        //private MainTabPage _mainTabPage;
+
+        public MainWindow(string title, int width = 500, int height = 200, bool hasMenubar = false) : base(title, width, height, hasMenubar)
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            _mainBox = new HorizontalBox();
+            this.Child = _mainBox;
+
+            _outerTab = new Tab();
+            _mainBox.Children.Add(_outerTab, true);
+            var s = new Area(new Program.AHandler());
+            _mainBox.Children.Add(s, true);
+            //_mainTabPage = new MainTabPage("Pages 1-5", _mainBox, this);
+            //_outerTab.Children.Add(_mainTabPage);
+        }
+    }
+}
---
+++ csharp/bnd/ZxGUI/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using DevZH.UI;
+using DevZH.UI.Drawing;
+/**
+ * Yeah , I exactly like coding with Java Style even though in a C# file.
+ */
+namespace bnd.ZxGUI{
+    /// <summary>
+    /// Yeah, I exactly don't like this kind of doc-comments...
+    /// wth for function extensions in CSharp???`public static` and `this`
+    /// </summary>
+    public static class MouseUtil{
+        public static bool IsUpEvent(this AreaMouseEvent e) {
+            return e.Up == 1;
+        }
+        public static bool IsDownEvent(this AreaMouseEvent e) {
+            return e.Down == 1;
+        }
+    }
+    
+    public static class Program{
+        public static void Main(string[] args) {
+            var app = new Application(false);
+            InitMenus(app);
+            var window = new MainWindow("啊♂乖乖站好", 320, 240, true) {
+                AllowMargins = true
+            };
+            app.Run(window);
+        }
+
+        public class AHandler : IAreaHandler{
+            public void Draw(AreaBase area, ref AreaDrawParams param) {
+            }
+
+            public void MouseEvent(AreaBase area, ref AreaMouseEvent mouseEvent) {
+                Console.WriteLine(mouseEvent.IsDownEvent() + "," + mouseEvent.IsUpEvent()+"(,"+mouseEvent.X+","+mouseEvent.Y+")");
+            }
+
+            public void MouseCrossed(AreaBase area, bool left) {
+            }
+
+            public void DragBroken(AreaBase area) {
+            }
+
+            public bool KeyEvent(AreaBase area, ref AreaKeyEvent keyEvent) {
+                Console.WriteLine(keyEvent.ToString());
+                return true;
+            }
+        }
+
+        private static void InitMenus(Application app) {
+            var file = new Menu("File");
+            file.Add("New");
+            file.Add("Open", MenuItemTypes.Common, obj => { Console.WriteLine(obj + ",23"); });
+            file.AddSeparator();
+            app.OnShouldExit += (sender, args) => {
+                Console.WriteLine("23333");
+                MessageBox.Show("23333");
+                app.Exit();
+            };
+            var edit = new Menu("Edit");
+            var undo = edit.Add("Undo");
+            undo.Enabled = false;
+            edit.AddSeparator();
+        }
+    }
+}
